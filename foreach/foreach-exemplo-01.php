9c405ece7ab80d1dcf0d9072fe43f9dacc738ff3
Anderson Carvalho de Souza <120695anderson@gmail.com>
2021-07-05 01:07:14 -0300

    exemplo de foreach e while/do while

---
+++ foreach/foreach-exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+
+    $messes = array("Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezenbro");
+
+    foreach($messes as $index => $mes){
+
+        echo "Index: ".$index."<br/>";
+        echo "O mês é ".$mes."<br/>";
+    }
+
+?>
