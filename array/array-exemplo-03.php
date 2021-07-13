874e5ceb6f626c899005b9a9ccfefcd594631f07
Anderson Carvalho de Souza <120695anderson@gmail.com>
2021-07-13 20:24:39 -0300

    array

---
+++ array/array-exemplo-03.php
@@ -0,0 +1,17 @@
+<?php
+
+    $pessoas = array();
+
+    array_push($pessoas, array(
+        'nome'=>'João',
+        'Idade' =>'20'
+    ));
+
+    array_push($pessoas, array(
+        'nome'=>'Glaucio',
+        'Idade' =>'25'
+    ));
+
+    print_r($pessoas);
+
+?>
